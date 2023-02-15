402972bd0454abf86b2d73817ea28d6960ba6b3b
kaisar fauzan <90669229+KaisarF@users.noreply.github.com>
2023-02-15 23:24:21 +0700

    Add files via upload

---
+++ components/RightContainerRequestPage.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="right-container">
+        <div class="first-row-right">
+            <SelectComponent title="ADN Type"/>
+            <SelectComponent title="ADN Price"/>
+        </div>
+        <div class="second-row-right">
+            <TextAreaComponent title="SMS Replay Succes" rows="8" />
+        </div>
+        <div class="third-row-right">
+            <TextAreaComponent title="SMS Replay Reject" rows="8" />
+        </div>
+        <div class="forth-row-right">
+            
+            <div id="input-area">
+                <InputComponent type="text" title="Data Label" placeholder="e.g SIK" />
+                <div >
+                    <ActionButton childclass="bg-green-500 w-10 h-10 rounded"  />
+                </div>
+                
+            </div>
+            
+        </div>
+        
+    </div>
+
+    
+</template>
+<script>
+import InputComponent from './InputComponent.vue';
+import SelectComponent from './SelectComponent.vue';
+import TextAreaComponent from './TextAreaComponent.vue';
+import ActionButton from './ActionButton.vue';
+
+export default {
+    name: 'RightContainer',
+    components:{
+        InputComponent,
+        SelectComponent,
+        TextAreaComponent,
+        ActionButton
+    }
+  }
+</script>
+<style scoped>
+.right-container{
+    padding: 40px;
+}
+
+    .first-row-right{
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        padding-bottom: 15px;
+    }
+    .third-row-right, .second-row-right{
+    padding-bottom: 15px;
+}
+
+    #input-area{
+        display: grid;
+        grid-template-columns: 6fr 1fr
+    }
+    .label-forth-row-right {
+      font-weight: 700;
+      font-size: 18px;
+      color: #7C7D80;
+      padding-bottom: 10px;
+      margin: 10px;
+      width: 100%;
+    }
+.add-button{
+    color: chartreuse;
+}
+
+</style>
---
+++ components/SelectComponent.vue
@@ -0,0 +1,58 @@
+
+<template>
+   <div>
+    <div class="select-card">
+      <label class="select-title">{{ title }}</label>
+      <select class="select-field" >
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="opel">Opel</option>
+        <option value="audi">Audi</option>
+      </select>
+    </div>
+   </div>
+    
+  </template>
+  
+  <script>
+    
+    export default {
+      name: 'RequestPage',
+      props: {
+        title: String,
+        type: String,
+        width:Number
+      }
+    }
+  </script>
+  <style scoped>
+  body{
+    background-color: white;
+  }
+  .select-title {
+      font-weight: 700;
+      font-size: 18px;
+      color: #7C7D80;
+      padding-bottom: 10px;
+      margin: 10px;
+      width: 100%;
+      
+    }
+    
+    .select-card {
+      display: flex;
+      flex-direction: column;
+      
+    }
+    .select-title {
+      text-align: start;
+    }
+    .select-field {
+      width: 90%;
+      height: 40px;
+      border: 1px solid #7C7D80;
+      
+      padding-left: 10px;
+      padding-right: 10px;
+    }
+</style>
